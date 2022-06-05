45a60ca5f32147eae8f6beaab99bdbe1084121b5
Adam Gaffney <adamgaffney96@gmail.com>
2022-06-05 14:57:52 +0100

    added functionality for the buttons to be clicked, and the modal to be closed (unfinished)

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,22 @@
 console.log("Script loaded successfully.");
 
 const cards = document.querySelectorAll(".card");
+const infoModal = document.querySelector(".display-modal");
+
+window.addEventListener("click", event => {
+    console.log(event.path);
+    if (!event.path.includes(infoModal) && infoModal.classList.contains("active-modal")) {
+        console.log('window');
+        infoModal.classList.add("transition-modal");
+        infoModal.classList.remove("active-modal");
+        setTimeout(() => { infoModal.classList.remove("transition-modal") },
+            300);
+    }
+}, true)
 
 cards.forEach(button => {
     button.addEventListener("click", event => {
-        if (button.getAttribute("style")) {
-            button.removeAttribute("style");
-            button.querySelector(".card-description").removeAttribute("style");
-        } else {
-            button.style.position = "absolute";
-            button.style.zIndex = "10";
-            button.querySelector(".card-description").style.display = "block";
-        }
-    })
-})
+        console.log('card');
+        infoModal.classList.add("active-modal");
+    }, false)
+});
